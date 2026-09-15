ba189fa4b1b7ea3110291349df5b2b4db2205dd6
Lê Trường <lehuytruong844@gmail.com>
2026-09-15 14:39:00 +0700

    Tạo các thư mục theo kiến trúc CleanArchitecture, tạo file docker và cấu hình sql server bằng EF core

---
+++ CyberManagement.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using CyberManagement.Infrastructure.Persistance;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CyberManagement.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString, b =>
+                    b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            return services;
+        }
+
+    }
+}
---
+++ CyberManagement.Infrastructure/Persistance/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CyberManagement.Infrastructure.Persistance
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Quét tự động các cấu hình bảng (EntityTypeConfiguration) nếu có
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
