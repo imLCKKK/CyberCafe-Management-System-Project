5e2c4bd459ced50451e8349ab831351f89e20faf
Lê Trường <lehuytruong844@gmail.com>
2026-09-20 09:55:38 +0700

    điều chỉnh lại namespace cho đúng folder

--- Backend/CyberManagement.Application/DTOs/InvoiceDTOs.cs
+++ Backend/CyberManagement.Application/DTOs/InvoiceDTOs.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace CyberManagement.Domain.Entities
+namespace CyberManagement.Application.DTOs
 {
     public class InvoiceDTOs
     {
