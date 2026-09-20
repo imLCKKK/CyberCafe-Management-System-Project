6b33ffd94e04d7b5306769fa5172776cce884a59
Lê Trường <lehuytruong844@gmail.com>
2026-09-20 16:39:45 +0700

    tạo DI riêng cho tầng application

---
+++ Backend/CyberManagement.Application/DependecyInjection.cs
@@ -0,0 +1,22 @@
+﻿using CyberManagement.Application.Interfaces.Services;
+using CyberManagement.Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CyberManagement.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<IOrderService, OrderService>();
+            services.AddScoped<IInvoiceService, InvoiceService>();
+
+            return services;
+        }
+    }
+}
